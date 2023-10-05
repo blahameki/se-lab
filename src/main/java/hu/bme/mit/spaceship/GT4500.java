54f1f6d2dc0425dcabfcc42530642f8ad59236aa
blahameki <blahameki03@gmail.com>
2023-10-05 10:36:37 +0200

    GT4500 new definition(case ALL in fireTorpedo())

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,13 +77,25 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
 
+        boolean primaryFired = false;
+        boolean secondaryFired = false;
+    
+        if (!primaryTorpedoStore.isEmpty()) {
+            primaryFired = primaryTorpedoStore.fire(1);
+        }
+    
+        if (!secondaryTorpedoStore.isEmpty()) {
+            secondaryFired = secondaryTorpedoStore.fire(1);
+        }
+    
+        firingSuccess = primaryFired || secondaryFired;
+    
+        wasPrimaryFiredLast = !wasPrimaryFiredLast;
+    
         break;
-    }
-
+      }
     return firingSuccess;
   }
-
 }
+
